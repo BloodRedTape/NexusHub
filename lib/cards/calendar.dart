24647f3ad8455bd948318f16094e8bb219a6d104
I.Kostiuk <gefestroyale@gmail.com>
2025-03-05 11:38:00 +0200

    Calendar does not overflow

--- lib/cards/calendar.dart
+++ lib/cards/calendar.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 import 'package:nexus/cards/base.dart';
 import 'package:nexus/cards/plain.dart';
 import 'package:nexus/cards/state.dart';
+import 'package:nexus/consts.dart';
 import 'package:nexus/states/calendar.dart';
 
 class CalendarDayWidget extends StatelessWidget {
@@ -16,57 +17,55 @@ class CalendarDayWidget extends StatelessWidget {
 
     double fontSize = 24;
 
-    return Padding(
-        padding: EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(children: [
-              Icon(Icons.calendar_month, size: 35),
-              const SizedBox(width: 8),
-              Text(
-                DateFormat('d MMMM').format(day.date),
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: fontSize),
-              ),
-            ]),
-            const SizedBox(height: 8),
-            Table(
-              columnWidths: {
-                0: IntrinsicColumnWidth(),
-                1: FixedColumnWidth(40.0),
-                2: FlexColumnWidth(),
-              },
-              children: day.events.map((event) {
-                final timeRangeText =
-                    '${event.start.hour}:${event.start.minute.toString().padLeft(2, '0')} - ${event.end.hour}:${event.end.minute.toString().padLeft(2, '0')}';
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(children: [
+          Icon(Icons.calendar_month, size: 35),
+          const SizedBox(width: 8),
+          Text(
+            DateFormat('d MMMM').format(day.date),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: fontSize),
+          ),
+        ]),
+        const SizedBox(height: 8),
+        Table(
+          columnWidths: {
+            0: IntrinsicColumnWidth(),
+            1: FixedColumnWidth(40.0),
+            2: FlexColumnWidth(),
+          },
+          children: day.events.map((event) {
+            final timeRangeText =
+                '${event.start.hour}:${event.start.minute.toString().padLeft(2, '0')} - ${event.end.hour}:${event.end.minute.toString().padLeft(2, '0')}';
 
-                return TableRow(
-                  children: [
-                    TableCell(
-                        child: Align(
-                      alignment: AlignmentDirectional.center,
-                      child: Text(timeRangeText, style: TextStyle(fontSize: fontSize)),
-                    )),
-                    const TableCell(
-                      verticalAlignment: TableCellVerticalAlignment.middle,
-                      child: Icon(
-                        Icons.circle,
-                        color: Colors.blue,
-                        size: 16,
-                      ),
-                    ),
-                    TableCell(
-                      child: Text(
-                        event.description,
-                        style: TextStyle(fontSize: fontSize, overflow: TextOverflow.ellipsis),
-                      ),
-                    ),
-                  ],
-                );
-              }).toList(),
-            ),
-          ],
-        ));
+            return TableRow(
+              children: [
+                TableCell(
+                    child: Align(
+                  alignment: AlignmentDirectional.center,
+                  child: Text(timeRangeText, style: TextStyle(fontSize: fontSize)),
+                )),
+                const TableCell(
+                  verticalAlignment: TableCellVerticalAlignment.middle,
+                  child: Icon(
+                    Icons.circle,
+                    color: Colors.blue,
+                    size: 16,
+                  ),
+                ),
+                TableCell(
+                  child: Text(
+                    event.description,
+                    style: TextStyle(fontSize: fontSize, overflow: TextOverflow.ellipsis),
+                  ),
+                ),
+              ],
+            );
+          }).toList(),
+        ),
+      ],
+    );
   }
 }
 
@@ -79,7 +78,16 @@ class CalendarCard extends StateCard<CalendarState> {
 
     return BaseCard(
       color: Colors.grey[900],
-      child: Padding(padding: EdgeInsets.all(8), child: Column(children: state.days.map((day) => CalendarDayWidget(day: day)).toList())),
+      child: Padding(
+        padding: EdgeInsets.all(cardPadding),
+        child: ListView.builder(
+          itemCount: state.days.length,
+          itemBuilder: (context, index) => Padding(
+            padding: EdgeInsets.only(bottom: cardPadding),
+            child: CalendarDayWidget(day: state.days[index]),
+          ),
+        ),
+      ),
     );
   }
 }
